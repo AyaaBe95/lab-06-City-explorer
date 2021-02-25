61f048692bc9165226cb6699d2b99d8738a0ed16
Aya berjawi <ayabarjawi@gmail.com>
2021-02-25 15:14:00 +0200

    lab8

--- server.js
+++ server.js
@@ -2,19 +2,16 @@
 
 require('dotenv').config();
 const express = require('express');
-const server = express();
 const cors = require('cors');
-const PORT = process.env.PORT || 3030;
-
-const pg =require('pg');
-const client=new pg.Client(process.env.DATABASE_URL);
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
-
-
-
 const superagent = require('superagent')
+const pg =require('pg');
+const PORT = process.env.PORT || 3030;
+const server = express();
 server.use(cors());
 
+
+const client=new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
 
 
 // location
@@ -26,8 +23,11 @@ server.get('/parks', parkHandler);
 
 function locationHandler(request, response) {
     const city = request.query.city;
+    console.log('gggg',request.query)
+
 
     getLocation(city)
+
         .then(locationData => {
             response.status(200).json(locationData);
 
@@ -39,44 +39,42 @@ function locationHandler(request, response) {
 }
 
 
-function getLocation(city,res) {
-    let lat;
-    let lon;
+function getLocation(city) {
+  
     let key = process.env.LOCATION_KEY;
-    let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
-    let SQL='SELECT * FROM location WHERE  search_query=$1;';
 
-     client.query(SQL)
-    .then(result =>{
-        console.log(result)
-        if (result.rowCount){
-            return result.row[0]
-        }
-        else{
-             superagent.get(url)
-            .then(locData => {
-                const SQLIn = 'INSERT INTO location(search_query,formatted_query,latitude,longitude)VALUES($1,$2,$3,$4) RETURNING *;';
-                var  locationData = new Location(city, locData.body);
-                lat=locationData.latitude;
-                lon=locationData.longitude;
-                let safevalues=[city,locationData.formatted_query,lat,lon];
-                 client.query(SQLIn, safevalues)
-                .then(result => {
-                    result.rows[0];
-                    res.send(result.rows);
-                })
-                .catch(error=>errorHandler(error));
-            })
 
-        }
-
-    })
-   
-
+    let SQL='SELECT * FROM locations WHERE  search_query=$1;';
+    let safevalue =[city]
     
 
-   
-    }
+     return client.query(SQL,safevalue)
+    .then(result =>{
+        if (result.rows.length ){
+            return result.rows[0]
+        }
+        else{
+            let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+             return superagent.get(url)
+            .then(locData => {
+                var  locationData = new Location(city, locData.body);
+                let formatted_query = locationData.formatted_query
+                let lat=locationData.latitude;
+                let lon=locationData.longitude;
+                const SQLIn = 'INSERT INTO locations (search_query,formatted_query,latitude,longitude)VALUES($1,$2,$3,$4) RETURNING *;';
+                let safevalues=[city,formatted_query,lat,lon];
+                return client.query(SQLIn, safevalues)
+                .then(result => {
+                    result.rows[0];
+
+            })
+            .catch(error=>errorHandler(error));
+
+        })
+
+        }
+    })
+}
 
 
 
